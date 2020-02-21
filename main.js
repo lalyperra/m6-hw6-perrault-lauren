c2d0e48ef9deb7a57505663f4e9cbcf80ee2cb02
lalyperra <35471457+lalyperra@users.noreply.github.com>
2020-02-21 12:26:10 -0800

    update javascript with assigned problem

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
-//Trigger for hover
-var navLinks = document.getElementsByClassName('nav-link');
+//click delivers message -- box1
+var box1 = document.getElementById('box1');
 
-//function applies to (show dropdown)
-var dropdowns = document.getElementsByClassName('dropdown');
+box1.addEventListener("click", myFunction);
 
+  function myFunction() {
+    alert("Oooh - so close, but no cigar");
+  }
 
-function toggleDropdown() {
-  this.children[1].classList.toggle('show');
-}
+ //box2
+var box2 = document.getElementById('box2');
 
-//event listeners for li li li li
-for (i=0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener('mouseenter', toggleDropdown);
-  navLinks[i].addEventListener('mouseleave', toggleDropdown);
-}
+box2.addEventListener("click", myFunction);
+  function myFunction() { 
+    alert("DING DING DING - We have a winner");
+  }
+
+  //box3
+var box3 = document.getElementById('box3');
+
+box3.addEventListener("click", myFunction);
+  function myFunction() {
+    alert("Oops, better luck next time");
+  }
+
+//click any box and remove hidden class to display Start Over message div
+var boxes = document.getElementById('boxes');
+
+document.addEventListener('click', myFunction);
+  function myFunction() {
+    document.getElementById("hide").style.display = "block";
+  }
+
+//change all the boxes backgrounds on any hover
+function  Myfunction {
+      boxes.hover(function() {
+      this.css("background-color", "yellow");
+      }
+    }
